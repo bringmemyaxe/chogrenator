8b0a09eca4a97ca2a4512fed7dc67344c0d19df5
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-07-05 22:27:12 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -7,38 +7,39 @@ Choose mode: ''')
         chosen_chords = []
         while waiting_for_an_acceptable_chord:
             temp_chord = input('add the first chord of your progression: ')
+            chord_type = temp_chord.replace(temp_chord[0], '')
             if temp_chord[0] == temp_chord:
                 chosen_chords.append(temp_chord)
-            else:
-                chord_type = temp_chord.replace(temp_chord[0], '')
-                if chord_type in known_chord_types:
-                    chosen_chords.append(temp_chord)
-                    waiting_for_an_acceptable_chord = False
-                    second_chord_added = False
-                    while not second_chord_added:
-                        temp_chord = input('add the next chord: ')
-                        if temp_chord[0] == temp_chord:
-                            chosen_chords.append(temp_chord)
-                        else:
-                            chord_type = temp_chord.replace(temp_chord[0], '')
-                            if chord_type in known_chord_types:
-                                chosen_chords.append(temp_chord)
-                                second_chord_added = True
-                                progression_finished = False
-                                while not progression_finished:
-                                    temp_chord = input('if there is another chord, add it. If there are no chords any more, write X: ')
-                                    if temp_chord == 'X':
-                                        progression_finished = True
-                                    else:
-                                        if temp_chord[0] == temp_chord:
-                                            chosen_chords.append(temp_chord)
-                                        else:
-                                            chord_type = temp_chord.replace(temp_chord[0], '')
-                                            if chord_type in known_chord_types:
-                                                chosen_chords.append(temp_chord)
-                                            else:
-                                                print('sorry... I do not know such chords yet. Maybe something more simple?')
+                chord_type = 'major'
+            if chord_type in known_chord_types:
+                chosen_chords.append(temp_chord)
+                waiting_for_an_acceptable_chord = False
+                second_chord_added = False
+                while not second_chord_added:
+                    temp_chord = input('add the next chord: ')
+                    chord_type = temp_chord.replace(temp_chord[0], '')
+                    if temp_chord[0] == temp_chord:
+                        chosen_chords.append(temp_chord)
+                        chord_type = 'major'
+                    if chord_type in known_chord_types:
+                        chosen_chords.append(temp_chord)
+                        second_chord_added = True
+                        progression_finished = False
+                        while not progression_finished:
+                            temp_chord = input(
+                                'if there is another chord, add it. If there are no chords any more, write X: ')
+                            if temp_chord == 'X':
+                                progression_finished = True
                             else:
-                                print('sorry... I do not know such chords yet. Maybe something more simple?')
-                else:
-                    print('sorry... I do not know such chords yet. Maybe something more simple?')
+                                chord_type = temp_chord.replace(temp_chord[0], '')
+                                if temp_chord[0] == temp_chord:
+                                    chosen_chords.append(temp_chord)
+                                    chord_type = 'major'
+                                if chord_type in known_chord_types:
+                                    chosen_chords.append(temp_chord)
+                                else:
+                                    print('sorry... I do not know such chords yet. Maybe something more simple?')
+                    else:
+                        print('sorry... I do not know such chords yet. Maybe something more simple?')
+            else:
+                print('sorry... I do not know such chords yet. Maybe something more simple?')
