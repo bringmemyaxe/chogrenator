cf8d5e346e0e773aafdad3c997c29385f4d0ee10
Robert <48741931+bringmemyaxe@users.noreply.github.com>
2020-07-06 16:17:31 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,18 @@
-major = [2, 2, 1, 2, 2, 2, 1]
-natural_minor = [2, 1, 2, 2, 1, 2, 2]
-harmonic_minor = [2, 1, 2, 2, 1, 3, 1]
-pentatonic_major = [2, 2, 3, 2, 3]
-pentatonic_minor = [3, 2, 2, 3, 2]
+tonics = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+extended_tonics = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#',
+                   'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G',]
+note_types = ['R', 'b2', '2', '3m', '3', '4', 'b5', '5', '#5', '6', '7', '7M']
+major = [2, 2, 1, 2, 2, 2]
+natural_minor = [2, 1, 2, 2, 1, 2]
+harmonic_minor = [2, 1, 2, 2, 1, 3]
+pentatonic_major = [2, 2, 3, 2]
+pentatonic_minor = [3, 2, 2, 3]
+scales = [major, natural_minor, harmonic_minor, pentatonic_major, pentatonic_minor]
 known_chord_types = ['m', 'sus2', 'major']
+chord_type_m = ['R', '3m', '5']
+chord_type_sus2 = ['R', '2', '5']
+chord_type_major = ['R', '3', '5']
+actual_chord_types = [chord_type_m, chord_type_sus2]
 while True:
     mode = input('''0 - add good chord progressions you like
 Choose mode: ''')
@@ -14,7 +23,6 @@ Choose mode: ''')
             temp_chord = input('add the first chord of your progression: ')
             chord_type = temp_chord.replace(temp_chord[0], '')
             if temp_chord[0] == temp_chord:
-                chosen_chords.append(temp_chord)
                 chord_type = 'major'
             if chord_type in known_chord_types:
                 chosen_chords.append(temp_chord)
@@ -24,7 +32,6 @@ Choose mode: ''')
                     temp_chord = input('add the next chord: ')
                     chord_type = temp_chord.replace(temp_chord[0], '')
                     if temp_chord[0] == temp_chord:
-                        chosen_chords.append(temp_chord)
                         chord_type = 'major'
                     if chord_type in known_chord_types:
                         chosen_chords.append(temp_chord)
@@ -38,7 +45,6 @@ Choose mode: ''')
                             else:
                                 chord_type = temp_chord.replace(temp_chord[0], '')
                                 if temp_chord[0] == temp_chord:
-                                    chosen_chords.append(temp_chord)
                                     chord_type = 'major'
                                 if chord_type in known_chord_types:
                                     chosen_chords.append(temp_chord)
@@ -49,8 +55,34 @@ Choose mode: ''')
                         chosen_chords_copy = chosen_chords.copy()
                         for chord_x in chosen_chords:
                             first_chord = chosen_chords_copy[0]
-                            second_chord = chosen_chords_copy[1]
-
+                            try:
+                                second_chord = chosen_chords_copy[1]
+                            except IndexError:
+                                second_chord = ''
+                            if second_chord != '':
+                                print('analyzing', first_chord, 'to', second_chord)
+                                for scale in scales:
+                                    for tonic in tonics:
+                                        scale_index = tonics.index(tonic)
+                                        current_scale = []
+                                        current_scale.append(tonic)
+                                        for step in scale:
+                                            scale_index += step
+                                            current_scale.append(extended_tonics[scale_index])
+                                        first_chord_notes = []
+                                        if first_chord[0] == first_chord:
+                                            first_tonic = tonics.index(first_chord)
+                                            for another_step in chord_type_major:
+                                                first_index = note_types.index(another_step)
+                                                current_note = first_tonic + first_index
+                                                first_chord_notes.append(extended_tonics[current_note])
+                                                print(first_chord_notes)
+                                        else:
+                                            first_tonic = tonics.index(first_chord[0])
+                                            print(tonics[first_tonic])
+                                            first_chord_type = temp_chord.replace(temp_chord[0], '')
+                                            for chord_type in actual_chord_types:
+                                                if first_chord_type == 
                             chosen_chords_copy.remove(chord_x)
                     else:
                         print('sorry... I do not know such chords yet. Maybe something more simple?')
